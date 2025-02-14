22180e7641a438c0e31fe597a12e15a763558d99
Samkwizera <s.ihimbazwe@alustudent.com>
2025-02-14 11:10:08 +0200

    updating patterns.py

--- patterns.py
+++ patterns.py
@@ -1,6 +1,7 @@
 import re
 # patterns.py
-email_pattern = re.compile(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")
-url_pattern = re.compile(r"^https?:\/\/(?:www\.)?[A-Za-z0-9.-]+\.[A-Za-z]{2,}\/?.*$")
-phone_pattern = re.compile(r"^\(?\d{3}\)?[-.]?\s?\d{3}[-.]?\s?\d{4}$")
-credit_card_pattern = re.compile(r"^(\d{4}[-\s]){3}\d{4}$")
+email_pattern = re.compile(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}")
+url_pattern = re.compile(r"https?:\/\/(?:www\.)?[A-Za-z0-9.-]+\.[A-Za-z]{2,}(?:\/\S*)?")
+phone_pattern = re.compile(r"\(?\d{3}\)?[-.]?\s?\d{3}[-.]?\s?\d{4}")
+credit_card_pattern = re.compile(r"(?:\d{4}[-\s]){3}\d{4}")
+
