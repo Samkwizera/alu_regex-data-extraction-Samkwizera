6e460ec52547cd7241485de24ee85d6a3a927e9b
Samkwizera <s.ihimbazwe@alustudent.com>
2025-02-14 11:27:29 +0200

    updated extractors.py

--- extractors.py
+++ extractors.py
@@ -3,16 +3,17 @@ from patterns import (
     url_pattern,
     phone_pattern,
     credit_card_pattern
+   
 )
-#  """Validate email addresses."""
+#  """Extracts and returns a list of email addresses."""
 def extract_emails(text):
     return email_pattern.findall(text)
-# """Validate URLs."""
+#  """Extract and return a list of URLs."""
 def extract_urls(text):
     return url_pattern.findall(text)
-# """Validate phone numbers."""
+#   """ Extract and returns a list of phone numbers """
 def extract_phone_numbers(text):
     return phone_pattern.findall(text)
-#   """Validate credit card numbers."""
+#   """Extracts and returns a list of credit card numbers."""
 def extract_credit_cards(text):
     return credit_card_pattern.findall(text)
