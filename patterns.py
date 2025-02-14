ae8e831f12bb54a13a22586e9b21c8e1355bff46
Samkwizera <s.ihimbazwe@alustudent.com>
2025-02-14 11:02:13 +0200

    update patterns.py

--- patterns.py
+++ patterns.py
@@ -1,5 +1,5 @@
 import re
-
+# patterns.py
 email_pattern = re.compile(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")
 url_pattern = re.compile(r"^https?:\/\/(?:www\.)?[A-Za-z0-9.-]+\.[A-Za-z]{2,}\/?.*$")
 phone_pattern = re.compile(r"^\(?\d{3}\)?[-.]?\s?\d{3}[-.]?\s?\d{4}$")
